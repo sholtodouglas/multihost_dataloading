acba6bd284fb743b0cef631eb7483aea9b73680e
Sholto Douglas <sholto.douglas1@gmail.com>
2022-09-06 10:32:04 +1000

    updated bash for loop to delete as well

--- workflow_setup/setup_hosts.py
+++ workflow_setup/setup_hosts.py
@@ -1,11 +1,10 @@
-from ipaddress import ip_address
 from os import system 
 import argparse
 import subprocess
 import requests
-import time
-import os
 
+OKGREEN = '\033[92m'
+ENDC = '\033[0m'
 
 parser = argparse.ArgumentParser(description='Setup slice workflow.')
 parser.add_argument('--tpu_name')
@@ -76,7 +75,7 @@ if __name__ == "__main__":
 
     # Copy file over to each TPU for setup
     try:
-      # Make the key we created authorised for all TPUs
+      # Make the key we created authorised for all TPUs TODO: Merge following two commands
       output = subprocess.run(f'gcloud compute tpus tpu-vm scp ~/.ssh/pod_key.pub {args.tpu_name}:.ssh/pod_key.pub --worker=all --zone={args.zone}', shell=True, check=True)
       # give all workers the private key - we could only give worker 0: TODO: Check security implications
       output = subprocess.run(f'gcloud compute tpus tpu-vm scp ~/.ssh/pod_key {args.tpu_name}:.ssh/pod_key --worker=all --zone={args.zone}', shell=True, check=True)
@@ -84,7 +83,7 @@ if __name__ == "__main__":
     except subprocess.CalledProcessError as error:
       print("Note - if this threw an error, as indicated it will likely be the fact that you haven't used scp before. Follow the fix provided in the gcp output, then rerun this script with --skip_ssh_connection to skip the ssh setup steps.")
 
-
+    print(f'{OKGREEN}SSH keys configured on all hosts {ENDC}')
   #############################################################################
   ########## Create a window per host & ssh directly into each one ############
   #############################################################################
@@ -113,57 +112,45 @@ if __name__ == "__main__":
     # and correct the permissions
     tmux_shell('chmod 600 ~/.ssh/pod_key')
   
-      # add all internal ips to eachother's known hosts, so that rsync works off the bat (avoids needing to unset strictHostChecking)
+    # add all internal ips to eachother's known hosts, so that rsync works off the bat (avoids needing to unset strictHostChecking)
     tmux_shell(f"ssh-keyscan -H {' '.join(internal_ips)} >> ~/.ssh/known_hosts")
     # If you wanted, you could now go to the tmux terminal of any of the machines, turn off broadcasting and then ssh into any other machine using
     #  'ssh -i ~/.ssh/pod_key.pub INSERT_INTERNAL_IP_HERE', where the internal ip addresses were printed earlier by our script (or you can look them up in gcp). 
     # This is what we will use to automatically synchronize files from host 0 to the others.
 
-    # modify this to change the watched working dir
+    # create a working dir to watch
     tmux_shell(f'mkdir {args.working_dir}')
     tmux_shell(f'cd {args.working_dir}')
-
+    print(f'{OKGREEN}Connected to all hosts {ENDC}')
   # #############################################################################
   # ########## Set up the fswatch sync across working dirs ############
   # #############################################################################
   if args.skip_setup_fswatch:
     print("Skipping fswatch - you're already syncing")
   else:
-  # create a new window, where we will launch fswatch from
+    # create a new window, where we will launch fswatch from
     tmux("new-window")
-    # connect to our main worker
+    # connect to our primary host
     tmux_shell(f"gcloud compute tpus tpu-vm ssh {args.tpu_name} --zone {args.zone} --project {args.project} --worker {0}")
 
     tmux_shell('sudo apt install fswatch')
 
-    # create the rsync file call sync.sh
+    # create the rsync file called sync.sh
     working_destinations = ' '.join([ip_address+':'+args.working_dir for ip_address in internal_ips[1:]])
-    ssh_access = "'ssh -i ~/.ssh/pod_key'"
-    file_create_string = f'echo "for d in {working_destinations}; do rsync -a -e {ssh_access} {args.working_dir} \$d; done" > sync.sh'
+
+    # do this quote escapery because others in the tmux shell string insertion it gets evaluated
+    # basically we need three levels of ' or " and the ssh key needs to be surrounded by ' ' as 
+    # does the bash for loop else one gets messed up during the transfer through the tmux shell.
+    ssh_access = '"\'"ssh -i ~/.ssh/pod_key"\'"'
+    file_create_string = f"echo 'for d in {working_destinations}; do rsync -a -e {ssh_access} {args.working_dir} \$d --delete; done' > sync.sh"
   
     tmux_shell(file_create_string)
     tmux_shell('chmod +x sync.sh')
 
-    # sync once every 3 seconds
+    # sync once every 3 seconds, recusively through the working dir, delete things in addition to adding
     tmux_shell(f'fswatch --one-per-batch --recursive --latency 3 --verbose {args.working_dir} | xargs -I{{}} ./sync.sh')
 
-    # go back
+    # go back, leave it running in window 1 - you can debug sync issues by switching back
     tmux("select-window -t 0")
 
-  #############################################################################
-  ########## Run setup scripts on each TPU ############
-  #############################################################################
-  
-
-  # Copy file over to each TPU for setup
-  # try:
-  #   output = subprocess.run(["gcloud", "compute", "tpus", "tpu-vm", "scp", "example.py", f"{args.tpu_name}:", "--worker=all", f"--zone={args.zone}" ], check=True)
-  #   print(output)
-  # except subprocess.CalledProcessError as error:
-  #   print("Note - if this threw an error, as indicated it will likely be the fact that you haven't used scp before. Follow the fix provided in the gcp output, then rerun this script with --skip_ssh_connection to skip the ssh setup steps.")
-
-  # for i in range(0, num_hosts):
-  #     tmux_select_pane(i)  
-  #     tmux_shell(f"python3 example.py")
-
-  # gcloud compute tpus tpu-vm scp ~/.ssh/pod_key.pub sholto-v2-32: --worker=all --zone=us-central1-a
+    print(f'{OKGREEN}Syncing! {ENDC}')

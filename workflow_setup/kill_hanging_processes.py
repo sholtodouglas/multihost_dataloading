3743b2fd20cbf63a176f0a5455a8eec77651943b
Sholto Douglas <sholto.douglas1@gmail.com>
2022-09-23 11:25:44 +1000

    kill tests

---
+++ workflow_setup/kill_hanging_processes.py
@@ -0,0 +1,19 @@
+from os import system
+import argparse
+import subprocess, signal
+import os
+
+
+parser = argparse.ArgumentParser(description='Kill hanging multihost programs.')
+parser.add_argument('--tpu_name')
+parser.add_argument('--zone')
+parser.add_argument('--project')
+parser.add_argument('--proc_name')
+
+
+if __name__ == "__main__":
+  args = parser.parse_args()
+  command = f"kill $(pgrep -f '{args.proc_name}')"
+  output = subprocess.run(f'gcloud compute tpus tpu-vm ssh {args.tpu_name} --zone {args.zone} --project {args.project} --worker=all --command {command}', shell=True, check=True)
+
+ 
